a4aa7d760d6b5f97b1babf7994f9efc67c13b851
joshgit <oopjosh@yahoo.com>
2012-10-13 15:13:42 -0700

    adding coinz
    coinz

--- start.js
+++ start.js
@@ -7,16 +7,38 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 var httpServer = app.listen(8080);
 
+var tiles_wide = 16;
+var tiles_high = 12;
+
+function randLoc() {
+	return [Math.floor(Math.random() * tiles_wide), Math.floor(Math.random() * tiles_high)];
+}
 
 var world = {};
 var pulse = Pulsar.createServer({server:httpServer});
 var room = pulse.channel('main');
+var coin_counts = {};
+
+function addCoin() {
+  world['coin'] = randLoc();
+  room.emit('coin', world['coin']);
+}
+addCoin();
+
+room.on('coin', function(img){
+  if (!coin_counts[img]) {
+	coin_counts[img] = 1;
+  }
+  else {
+	coin_counts[img]++;
+  }
+  addCoin();
+});
 
 room.on('join', function(img){
-  var x = 0;
-  var y = 0;
-  world[img] = [x,y];
-  room.emit('join', img, x, y);
+  var loc = randLoc();
+  world[img] = [loc[0], loc[1]];
+  room.emit('join', img, loc[0], loc[1]);
 });
 
 room.on('move', function(img, x, y){
