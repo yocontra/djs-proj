381d84b8fe1a7b027ad969fb172361ecf9629b39
joshgit <oopjosh@yahoo.com>
2012-10-13 16:19:53 -0700

    handling receiving snake events from server

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,23 @@ room.emit('join', 'my image url');
 
 room.on('join', function(img, x, y){
   //Add snake of ID img and set pos
+  var snake_div = $("<div id=\"" + img + "\" class=\"snake\"></div>");
+  snake_div.css("top", x + "px");
+  snake_div.css("left", y + "px");
+  $("#main").append(snake_div);
 });
 
 room.on('move', function(img, x, y){
   //Move other persons snake of ID img
+  var existing_snake = $("#" + img);
+  if (existing_snake.length) {
+	existing_snake.css("top", x + "px");
+	existing_snake.css("left", y + "px");
+  }
 });
 
 room.on('leave', function(img){
   //Delete snake of ID img
+  $("#" + img).remove();
 });
+
